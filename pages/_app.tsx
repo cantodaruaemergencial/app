bd885f8165cab9f8d58e48984b75af0b8c534976
frattezi <pfrattezi@gmail.com>
2021-03-21 17:54:48 -0300

    feat: setup theme for application

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,31 +1,25 @@
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { createMuiTheme, responsiveFontSizes } from '@material-ui/core/styles';
 import ThemeProvider from '@material-ui/styles/ThemeProvider';
 import type { AppProps } from 'next/app';
-import { useState } from 'react';
+import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }: AppProps): React.ReactElement {
-  const [darkState, setDarkState] = useState(false);
-  const palletType = darkState ? 'dark' : 'light';
+import DefaultTheme from '#/utils/theme';
 
-  const theme = createMuiTheme({
-    palette: {
-      type: palletType,
-    },
-  });
-
-  const handleThemeChange = () => {
-    setDarkState(!darkState);
-  };
-
-  const responsiveTheme = responsiveFontSizes(theme);
+const MyApp = ({ Component, pageProps }: AppProps): React.ReactElement => {
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side');
+    if (jssStyles) {
+      jssStyles.parentElement?.removeChild(jssStyles);
+    }
+  }, []);
 
   return (
-    <ThemeProvider theme={responsiveTheme}>
+    <ThemeProvider theme={DefaultTheme}>
       <CssBaseline />
-      <Component {...pageProps} handleThemeChange={handleThemeChange} />
+      <Component {...pageProps} />
     </ThemeProvider>
   );
-}
+};
 
 export default MyApp;
---
+++ pages/_document.tsx
@@ -0,0 +1,50 @@
+import { ServerStyleSheets } from '@material-ui/core/styles';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+import { Children } from 'react';
+
+import DefaultTheme from '#/utils/theme';
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          {/* PWA primary color */}
+          <meta
+            name="theme-color"
+            content={DefaultTheme.palette.primary.main}
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+MyDocument.getInitialProps = async (ctx) => {
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
+
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
+    });
+
+  const initialProps = await Document.getInitialProps(ctx);
+
+  return {
+    ...initialProps,
+    // Styles fragment is rendered after the app and page rendering finish.
+    styles: [
+      ...Children.toArray(initialProps.styles),
+      sheets.getStyleElement(),
+    ],
+  };
+};
