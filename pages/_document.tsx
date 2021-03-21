0e064a12eee839d1d97f5f4a96972c8274c7acb6
Marco Antonio <ma.souza.junior@gmail.com>
2021-03-21 19:43:41 -0300

    enh: add PWA metadata

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,19 +1,56 @@
 import { ServerStyleSheets } from '@material-ui/core/styles';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { Children } from 'react';
+import { Children, ReactElement } from 'react';
 
 import DefaultTheme from '#/utils/theme';
 
+const APP_NAME = 'next-pwa example';
+const APP_DESCRIPTION = 'This is an example of using next-pwa plugin';
+
+const pwaMetaData = (
+  <>
+    {/* PWA primary color */}
+    <meta name="theme-color" content={DefaultTheme.palette.primary.main} />
+
+    {/* TODO: improve meta data */}
+    <meta name="application-name" content={APP_NAME} />
+    <meta name="apple-mobile-web-app-capable" content="yes" />
+    <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+    <meta name="apple-mobile-web-app-title" content={APP_NAME} />
+    <meta name="description" content={APP_DESCRIPTION} />
+    <meta name="format-detection" content="telephone=no" />
+    <meta name="mobile-web-app-capable" content="yes" />
+
+    {/* TIP: set viewport head meta tag in _app.js, otherwise it will show a warning */}
+    {/* <meta name='viewport' content='minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover' /> */}
+
+    <link
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/icons/apple-touch-icon.png"
+    />
+    <link rel="manifest" href="/manifest.json" />
+    <link rel="shortcut icon" href="/icons/favicon.ico" />
+    <style>{`
+      html, body, #__next {
+        height: 100%;
+      }
+      #__next {
+        margin: 0 auto;
+      }
+      h1 {
+        text-align: center;
+      }
+      `}</style>
+  </>
+);
+
 export default class MyDocument extends Document {
-  render() {
+  render(): ReactElement {
     return (
       <Html lang="en">
         <Head>
-          {/* PWA primary color */}
-          <meta
-            name="theme-color"
-            content={DefaultTheme.palette.primary.main}
-          />
+          {pwaMetaData}
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
