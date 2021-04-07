14ce6d9157c79d80d4e9a665e1beaa1f70eb25b7
Marco Antônio <marco@paradigm.co>
2021-04-07 14:28:23 -0300

    enh: improve navbar

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,20 +1,19 @@
 import AppBar from '@material-ui/core/AppBar';
-import IconButton from '@material-ui/core/IconButton';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme: Theme) =>
+const useStyles = makeStyles(() =>
   createStyles({
     root: {
       flexGrow: 1,
     },
-    menuButton: {
-      marginRight: theme.spacing(2),
-    },
     title: {
       flexGrow: 1,
+    },
+    logoImage: {
+      width: '4rem',
+      padding: '.5rem',
     },
   }),
 );
@@ -26,14 +25,11 @@ export default function ButtonAppBar(): React.ReactElement {
     <div className={classes.root}>
       <AppBar position="static" color="default">
         <Toolbar>
-          <IconButton
-            edge="start"
-            className={classes.menuButton}
-            color="inherit"
-            aria-label="menu"
-          >
-            <MenuIcon />
-          </IconButton>
+          <img
+            className={classes.logoImage}
+            src="images/helpingHand.png"
+            alt="logo"
+          />
           <Typography variant="h6" className={classes.title}>
             Canto da Rua
           </Typography>
