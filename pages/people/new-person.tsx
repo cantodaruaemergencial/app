a6ee3bb4bbbebdda19ad3e86f0a0e9bea333602e
vinicius.felicissimo <vinicius.felicissimo@dtidigital.com.br>
2021-05-17 09:34:01 -0300

    Login fixes

--- pages/people/new-person.tsx
+++ pages/people/new-person.tsx
@@ -1,52 +1,27 @@
-// import Layout from '#/components/Layout';
-// import PersonPage from '#/components/Pages/PersonPage';
-// import PeopleService from '#/services/PeopleService';
-// import { Form } from '#/types/Forms';
-
-// interface Props {
-//   form: Form;
-// }
-
-// const NewPerson = ({ form }: Props): React.ReactElement => (
-//   <Layout title="Cadastro - Canto da Rua">
-//     <PersonPage form={form} />
-//   </Layout>
-// );
-
-// export const getStaticProps = async () => {
-//   const form = await PeopleService.getPersonForm();
-
-//   return {
-//     props: {
-//       form,
-//     },
-//     revalidate: 60,
-//   };
-// };
-
-// export default NewPerson;
-
-// TODO: REMOVER APÓS CORRIGIR BUILD
-
-import { useEffect, useState } from 'react';
-
 import Layout from '#/components/Layout';
 import PersonPage from '#/components/Pages/PersonPage';
 import PeopleService from '#/services/PeopleService';
 import { Form } from '#/types/Forms';
 
-const NewPerson = (): React.ReactElement => {
-  const [form, setForm] = useState<Form>({ sections: [] });
+interface Props {
+  form: Form;
+}
 
-  useEffect(() => {
-    PeopleService.getPersonForm().then((f) => setForm(f));
-  }, []);
+const NewPerson = ({ form }: Props): React.ReactElement => (
+  <Layout title="Cadastro - Canto da Rua">
+    <PersonPage form={form} />
+  </Layout>
+);
 
-  return (
-    <Layout title="Cadastro - Canto da Rua">
-      {form && <PersonPage form={form} />}
-    </Layout>
-  );
+export const getStaticProps = async () => {
+  const form = await PeopleService.getPersonForm();
+
+  return {
+    props: {
+      form,
+    },
+    revalidate: 60,
+  };
 };
 
 export default NewPerson;
